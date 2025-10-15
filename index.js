e2cb30ac47c8512c5d72db78466b0a6f6fc09c83
guardianprime <okongordian@gmail.com>
2025-10-15 18:45:27 +0100

    chore: created all the foundational code

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
+import { timeStamp } from "console";
 
+dotenv.config();
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.get("/", (req, res) => {
-  res.send("HNG has started!!!!");
+  res.send({ message: "hng has started" });
 });
 
-app.listen(3000, () => {
-  console.log("server is running on cool temp!!");
+app.get("/me", (req, res) => {
+  res.send({
+    status: "success",
+    user: {
+      email: "okongordian",
+      name: "gordian okon",
+      stack: "Nodejs",
+    },
+    timestamp: "current utc time in iso 8601 format",
+    fact: "random cat fact from cat facts API",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running on cool temp!! @ ${PORT} `);
 });
